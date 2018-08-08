429a82025748872729cbd367b6908736fe99134c
Dylan <30002799@g.toiohomai.ac.nz>
2018-08-08 18:56:31 +1200

    Needs work. Cannot display properties.

---
+++ ObjectProject1/BankCustomers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectProject1
+{
+    public class BankCustomers
+    {
+        float accountNumber;
+        float pin;
+        string firstName;
+        string lastName;
+
+        public float AccountNumber
+        {
+            get => accountNumber; set => accountNumber = value;
+        }
+        public float Pin
+        {
+            get => pin; set => accountNumber = value;
+        }
+        public string FirstName
+        {
+            get => firstName; set => firstName = value;
+        }
+        public string LastName
+        {
+            get => lastName; set => firstName = value;
+        }
+        public BankCustomers(float _AccountNumber, float _Pin, string _FirstName, string _LastName)
+        {
+            AccountNumber = _AccountNumber;
+            Pin = _Pin;
+            FirstName = _FirstName;
+            LastName = _LastName;
+        }
+    }
+}
---
+++ ObjectProject1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankCustomers customer1 = new BankCustomers(12345, 1111, "John", "Smith");
+            List<BankCustomers> List1 = new List<BankCustomers>
+            {
+                customer1
+            };
+            foreach(var customer in List1)
+            {
+                Console.WriteLine($"First Name: {customer.FirstName}\nLast Name: {customer.LastName}\nAccount Number: {customer.AccountNumber}\nPIN: {customer.Pin}");
+            }
+        }
+    }
+}
